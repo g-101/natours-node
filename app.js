fda66a00c92ca50e0abe8d793cf933323851ccc2
genesis-g101 <gyreplicant@gmail.com>
2022-12-14 10:56:00 -0300

    criando o get request

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 const port = 3333;
 
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/data/tours-simple.json`));
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({ message: 'sucess', results: tours.length, data: tours });
+});
+
+// app.post('/', (req, res) => {
+//   res.status(200).json({ message: 'Você pode postar neste endpoint!' });
+// });
 app.listen(port, () => console.log(`http://localhost:${port}`));
