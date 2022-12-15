4360450378f97ea4cdc4419bf481f7a65e95e27c
genesis-g101 <gyreplicant@gmail.com>
2022-12-14 21:32:31 -0300

    refactor: routes

--- app.js
+++ app.js
@@ -9,24 +9,30 @@ app.use(express.json());
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/data/tours-simple.json`));
 
-app.post('/api/v1/tours', (req, res) => {
-  let id = tours.length - 1;
-  id = id + 1;
+const createTour = (req, res) => (req, res) => {
+  const id = tours[tours.length - 1].id + 1;
 
-  // const { name, duration, difficulty } = req.body;
   const newTour = { id, ...req.body };
 
   tours.push(newTour);
   fs.writeFile(`${__dirname}/data/tours-simple.json`, JSON.stringify(tours), err => {
     res.status(201).send({ message: 'created', data: newTour });
   });
-});
+};
 
-app.get('/api/v1/tours', (req, res) => {
+const getAllTours = (req, res) => {
   res.status(200).send({ message: 'OK', results: tours.length, data: tours });
-});
+};
 
-app.put('/api/v1/tours/:id', (req, res) => {
+const getTour = (req, res) => {
+  const { id } = req.params;
+
+  const index = tours.findIndex(tour => tour.id === +id);
+
+  res.status(200).send({ message: 'OK', data: tours[index] });
+};
+
+const updateTour = (req, res) => {
   const { id } = req.params;
 
   const index = tours.findIndex(tour => tour.id === +id);
@@ -36,15 +42,19 @@ app.put('/api/v1/tours/:id', (req, res) => {
   fs.writeFile(`${__dirname}/data/tours-simple.json`, JSON.stringify(tours), err => {
     res.status(200).send({ message: 'updated - OK', data: tours[index] });
   });
-});
-app.delete('/api/v1/tours/:id', (req, res) => {
+};
+
+const deleteTour = (req, res) => {
   const { id } = req.params;
   const index = tours.findIndex(tour => tour.id === +id);
   tours.splice(index, 1);
 
   fs.writeFile(`${__dirname}/data/tours-simple.json`, JSON.stringify(tours), err => {
-    res.status(204).send('deleted');
+    res.status(204).send(null);
   });
-});
+};
+
+app.route('/api/v1/tours').post(createTour).get(getAllTours);
+app.route('/api/v1/tours/:id').get(getTour).put(updateTour).delete(deleteTour);
 
 app.listen(port, () => console.log(`http://localhost:${port}`));
